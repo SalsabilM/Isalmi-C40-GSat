a139563535a0ad5c495fa7f8a5a40cb431a64871
Salsabil mohamed <salsabilmohamed128@gmail.com>
2024-09-29 12:58:05 +0300

    tasbeeh code

--- app/src/main/java/com/route/islamic40gsat/fragments/TasbeehFragment.kt
+++ app/src/main/java/com/route/islamic40gsat/fragments/TasbeehFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.route.islamic40gsat.R
 
@@ -14,11 +17,28 @@ class TasbeehFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tasbeeh, container, false)
+        return inflater.inflate(
+            R.layout.fragment_tasbeeh,
+            container, false
+        )
     }
+    lateinit var tasbehBTN : Button
+    lateinit var sebhaIV : ImageView
+    lateinit var numtasbehTV : TextView
+    var counterTasbeh = 0
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        tasbehBTN = view.findViewById(R.id.btn_tasbeh)
+        sebhaIV = view.findViewById(R.id.iv_body_sebha)
+        numtasbehTV = view.findViewById(R.id.tv_sebha_counter)
+        tasbehBTN.setOnClickListener(View.OnClickListener {
+            counterTasbeh++
+            numtasbehTV.text = counterTasbeh.toString()
+        })
+        sebhaIV.setOnClickListener(View.OnClickListener {
+            counterTasbeh++
+            numtasbehTV.text = counterTasbeh.toString()
+        })
     }
 }
